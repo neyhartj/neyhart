d51985534534e4164c924284466a666892cc8dc7
neyhartj <neyha001@umn.edu>
2016-06-22 08:37:14 -0500

    Major changes

--- R/UM_colors.R
+++ R/UM_colors.R
@@ -6,5 +6,29 @@
 #'
 #' @source \url{https://www.ur.umn.edu/brand/assets/pdf/secondary_colors_rgb.pdf}
 #'
+"UMN.colors"
+
+
+#' University of Minnesota Color Generator
 #'
-"UM.colors"
+#' @description Generates hexidecimal colors from the University of Minnesota
+#' color palette.
+#'
+#' @param n Integer number of colors to be generated
+#'
+#' @return A character vector of hexidecimal color codes from the University
+#' of Minnesota color palette.
+#'
+#' @details
+#'
+#' @export
+#'
+
+UMN.color.palette <- function(n) {
+  #Error
+  if (n < 1) stop("n cannot be less than 1.")
+  if (n > length(UMN.colors)) stop("n cannot be greater than the total length of the palette.")
+
+  return(UMN.colors[1:n])
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # miscRutils
 A package of my personal random utilities in R
+
+## Installation
+To install, run the following command (this assumes you have the R package 
+`devtools` installed):
+
+```
+devtools::install_github("neyhartj/miscRutils")
+```
