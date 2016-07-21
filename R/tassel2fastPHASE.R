9bd80a96a620ad2ac57c2a531c96d565bcae2155
neyhartj <neyha001@umn.edu>
2016-07-21 10:00:49 -0500

    Added tassel to fastPHASE file conversion

---
+++ R/tassel2fastPHASE.R
@@ -0,0 +1,14 @@
+#' Convert between TASSEL hapmap file and a fastPHASE input
+#'
+#' @description
+#' Converts from a TASSEL hapmap file, which typically has diploid encoding of
+#' genotypes (i.e. AA or CC or AC) to an input file for fastPHASE, which is formatted
+#' line-by-line and contains information that is more haplotype-based.
+#'
+#'
+#' @export
+#'
+#'
+tassel2fastPHASE <- function(hapmap) {
+  
+}
