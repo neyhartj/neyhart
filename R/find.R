8ceae70955c2fe8b553a8de8332cc6ca4db05478
Jeff Neyhart <neyhartje@gmail.com>
2016-09-13 15:25:54 -0500

    Changes. Introduced a find function that allows for specific classes of objects to be found

---
+++ R/find.R
@@ -0,0 +1,38 @@
+#' Find objects by name and class
+#' 
+#' @description 
+#' Wrapper of the \code{\link[utils]{apropos}} function for finding objects, but
+#' includes the ability to find objects based on class.
+#' 
+#' @param what See \code{\link[utils]{apropos}}
+#' @param ignore.case See \code{\link[utils]{apropos}}
+#' @param class character string with a class. Objects of class \code{class} will
+#' be returned.
+#' 
+#' @export
+#' 
+find <- function(what, ignore.case = TRUE, class = "any") {
+  
+  # Error handling
+  if (!is.logical(ignore.case)) stop("'ignore.case' must be logical.")
+  if (!is.character(class)) stop("'class' must be character.")
+  
+  # Wrap around the 'apropos' function
+  objects <- apropos(what = what, ignore.case = ignore.case, mode = "any")
+  
+  # Determine the classes of those objects
+  object.class <- sapply(X = objects, FUN = function(obj)
+    class(get(obj)) )
+  
+  # If 'any' class is desired, return all objects
+  if (class == "any") {
+    return(objects)
+    
+    # Else subset the objects by the desired class
+  } else {
+    desired.objects <- objects[sapply(object.class, FUN = function(classes) class %in% classes)]
+    return(desired.objects)
+    
+  }
+  
+} # Close the function
