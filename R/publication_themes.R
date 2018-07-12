754d7fdd6587ec29a2ec235956c937472fd120d7
neyhartj <neyhartje@gmail.com>
2018-07-12 15:27:44 -0500

    Many edits

--- R/publication_themes.R
+++ R/publication_themes.R
@@ -25,11 +25,14 @@ theme_poster <- function() {
 #' @description 
 #' Themes for ggplot2 designed for compliance with variance scientific journals.
 #' 
+#' @rdname themes
+#' 
 #' @details 
 #' 
 #' \describe{
-#'   \item{theme_acs}{Theme for the American Society of Agronomy (ASA), Crop Science Society of America (CSSA), and
-#'   Soil Science Society of America (SSSA).}
+#'   \item{\code{theme_acs}}{Theme for the American Society of Agronomy (ASA), 
+#'   Crop Science Society of America (CSSA), and Soil Science Society of America (SSSA).}
+#'   \item{\code{theme_pnas}}{Theme for the Proceedings of the National Academy of Sciences.}
 #' }
 #' 
 #' @import ggplot2
@@ -43,3 +46,24 @@ theme_acs <- function(base_size = 8) {
     theme(panel.grid = element_blank())
   
 }
+
+
+#'
+#' @rdname themes
+#' 
+#' @export
+#' 
+theme_pnas <- function(base_size = 6) {
+  
+  theme_classic(base_size = base_size) %+replace%
+  theme(panel.grid = element_blank(),
+        strip.background = element_rect(fill = "grey85", size = 0),
+        axis.title = element_text(size = 6),
+        legend.text = element_text(size = 6))
+  
+}
+
+
+
+
+
